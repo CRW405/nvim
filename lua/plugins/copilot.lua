ddf4966aea26519c82fb777a1f6f2d8b6cd690df
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-04-08 13:25:33 -0500

    changed my mind for the 10th time

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,30 +1,28 @@
--- vim.g.copilot_enabled = 0
--- -- vim.g.copilot_no_tab_map = true
---
--- return {
--- 	"github/copilot.vim",
--- 	event = "InsertEnter",
--- 	config = function()
--- 		-- vim.keymap.set("i", "<C-Space>", "<Plug>(copilot-suggest)", { desc = "Trigger Copilot Suggest" })
--- 		-- vim.keymap.set("i", "<C-Tab>", "<Plug>(copilot-next)", { desc = "Next Copilot Suggestion" })
--- 		-- vim.keymap.set("i", "<C-S-Tab>", "<Plug>(copilot-previous)", { desc = "Previous Copilot Suggestion" })
--- 		-- vim.keymap.set(
--- 		-- 	"i",
--- 		-- 	"<C-S-Tab>",
--- 		-- 	"<Plug>(copilot-accept)",
--- 		-- 	{ desc = "Accept Copilot Suggestion", remap = false, silent = true }
--- 		-- )
---
--- 		vim.keymap.set("n", "<leader>ct", function()
--- 			if vim.g.copilot_enabled == 1 then
--- 				vim.cmd("Copilot disable")
--- 				vim.notify("Copilot Suggestions Disabled.", vim.log.levels.INFO)
--- 			else
--- 				vim.cmd("Copilot enable")
--- 				vim.notify("Copilot Suggestions Enabled.", vim.log.levels.INFO)
--- 			end
--- 		end, { desc = "Toggle Copilot" })
--- 	end,
--- }
+vim.g.copilot_enabled = 0
+-- vim.g.copilot_no_tab_map = true
 
-return {}
+return {
+	"github/copilot.vim",
+	event = "InsertEnter",
+	config = function()
+		-- vim.keymap.set("i", "<C-Space>", "<Plug>(copilot-suggest)", { desc = "Trigger Copilot Suggest" })
+		-- vim.keymap.set("i", "<C-Tab>", "<Plug>(copilot-next)", { desc = "Next Copilot Suggestion" })
+		-- vim.keymap.set("i", "<C-S-Tab>", "<Plug>(copilot-previous)", { desc = "Previous Copilot Suggestion" })
+		-- vim.keymap.set(
+		-- 	"i",
+		-- 	"<C-S-Tab>",
+		-- 	"<Plug>(copilot-accept)",
+		-- 	{ desc = "Accept Copilot Suggestion", remap = false, silent = true }
+		-- )
+
+		vim.keymap.set("n", "<leader>ct", function()
+			if vim.g.copilot_enabled == 1 then
+				vim.cmd("Copilot disable")
+				vim.notify("Copilot Suggestions Disabled.", vim.log.levels.INFO)
+			else
+				vim.cmd("Copilot enable")
+				vim.notify("Copilot Suggestions Enabled.", vim.log.levels.INFO)
+			end
+		end, { desc = "Toggle Copilot" })
+	end,
+}
