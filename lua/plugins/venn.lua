af71938e0749b6a899fa60fcec14237bd8f62f17
CRW405 <134819932+CRW405@users.noreply.github.com>
2026-05-13 17:46:25 -0500

    added ascii drawing

--- lua/plugins/venn.lua
+++ lua/plugins/venn.lua
@@ -11,6 +11,11 @@ return {
 				vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", { noremap = true })
 				vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", { noremap = true })
 				vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", { noremap = true })
+				-- draw with arrow keys
+				vim.api.nvim_buf_set_keymap(0, "n", "<S-Down>", "<C-v>j:VBox<CR>", { noremap = true })
+				vim.api.nvim_buf_set_keymap(0, "n", "<S-Up>", "<C-v>k:VBox<CR>", { noremap = true })
+				vim.api.nvim_buf_set_keymap(0, "n", "<S-Right>", "<C-v>l:VBox<CR>", { noremap = true })
+				vim.api.nvim_buf_set_keymap(0, "n", "<S-Left>", "<C-v>h:VBox<CR>", { noremap = true })
 				-- draw a box by pressing "f" with visual selection
 				vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", { noremap = true })
 			else
@@ -20,6 +25,10 @@ return {
 				vim.api.nvim_buf_del_keymap(0, "n", "L")
 				vim.api.nvim_buf_del_keymap(0, "n", "H")
 				vim.api.nvim_buf_del_keymap(0, "v", "f")
+				vim.api.nvim_buf_del_keymap(0, "n", "<S-Down>")
+				vim.api.nvim_buf_del_keymap(0, "n", "<S-Up>")
+				vim.api.nvim_buf_del_keymap(0, "n", "<S-Right>")
+				vim.api.nvim_buf_del_keymap(0, "n", "<S-Left>")
 				vim.b.venn_enabled = nil
 			end
 		end
